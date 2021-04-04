f42c757c7f976120ed74ead25b9f21a767802dcf
antoine <antoine.raoult3@etu.univ-lorraine.fr>
2021-04-04 18:52:56 +0200

    plic 2

--- src/plic/repint/Affectation.java
+++ src/plic/repint/Affectation.java
@@ -15,7 +15,7 @@ public class Affectation extends Instruction {
     void verifier() throws ErreurSemantique {
         acces.verifier();
         expression.verifier();
-        //if(expression.getType().equals("booleen")) throw new ErreurSemantique("dans une affectation, le membre de droite doit être de type entier");
+        if(expression.getType().equals("booleen")) throw new ErreurSemantique("dans une affectation, le membre de droite doit être de type entier");
     }
 
     @Override
